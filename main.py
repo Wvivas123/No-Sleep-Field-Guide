ce66e2c19f959731875a44065eff91a97e8c7cba
William Vivas <williamvivasdev@gmail.com>
2022-10-03 15:19:05 -0400

    added IKE need to finish it

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def print_hi(name):
         b = open("Field_Guide/SNMP.txt", "r")
         print(b.read())
 
+    if protocol == 'ike':
+        b = open("Field_Guide/IKE.txt", "r")
+        print(b.read())
 
-
-
-   # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
